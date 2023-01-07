5e29fe8b154530d642d31c2d70dae804b064aa41
Fakhrul Islam Sojib <fakhrulsojib@gmail.com>
2023-01-07 15:28:44 +0600

    Added username feature and showed entry and leaving to others on chatbox

--- Server/index.js
+++ Server/index.js
@@ -3,19 +3,60 @@ var n = new server({port: 4100});
 
 n.on('connection', function(ws) {
 	ws.on('message', function(message) {
-		console.log("got one message: " + message);
-		ws.send("User1: " + message);
+
+		message = JSON.parse(message);
+		// console.log("asce: ", message.type);
+		if (message.type == "naming") {
+			ws.parsonName = message.data;
+			n.clients.forEach(function e(client) {
+				if (client != ws) {
+					client.send(JSON.stringify({
+						type: "dhukaorberhowa",
+						name: message.data,
+						data: "joined the chat."
+					}));
+				} else {
+					client.send(JSON.stringify({
+						type: "userList",
+						name: message.data,
+						data: n.clients.size
+					}));
+				}
+			});
+			return;
+		}
+		// console.log("got one message: " + message.data + " " + ws.parsonName);
+		var nam = ws.parsonName;
+		n.clients.forEach(function e(client) {
+			// console.log(nam);
+			if (client != ws) {
+				client.send(JSON.stringify({
+					type: "message",
+					name: nam,
+					data: message.data
+				}));
+			}
+		});
+
+		// ws.send("User1: " + message);
 	});
 
-	ws.on('close', function() {
-		console.log("One more gone!");
-		ws.send('User1 left.');
-	});
-
-	console.log("Asche ekjon user");
-	ws.send('User1 has joined the chat!');
 	// ws.on('close', function() {
-	// 	console.log("");
-	// 	ws.send('');
+	// 	console.log("One more gone!");
+	// 	ws.send('User1 left.');
 	// });
+
+	// console.log("Asche ekjon user");
+	ws.on('close', function() {
+		var nam = ws.parsonName;
+		n.clients.forEach(function e(client) {
+			if (client != ws) {
+				client.send(JSON.stringify({
+					type: "dhukaorberhowa",
+					name: nam,
+					data: "left the chat."
+				}));
+			}
+		});
+	});
 });
